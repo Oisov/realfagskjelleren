7469c9c867a8b68aea3fe895d5c025bbea3f5336
Knutner <knut.nerga@gmail.com>
2017-10-21 20:39:44 +0200

    Laget grunnoppsett for mobildesign

---
+++ javascript/navigationbar.js
@@ -0,0 +1,9 @@
+document.getElementById("menu_button").onclick = function() {
+  displayMenu()
+};
+
+function displayMenu(){
+
+  // document.getElementById("testScum").style.display = "block";
+
+}
