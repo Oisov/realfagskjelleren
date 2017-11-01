46b1850e4f7a4b91002d05d02deb2965ba85bede
simonaso <simonaso@users.noreply.github.com>
2017-11-01 20:45:33 +0100

    konsept demo av finn toern

---
+++ javascript/games.js
@@ -0,0 +1,10 @@
+function openFinnToern() {
+  var modal = document.getElementById("finnToernModal");
+  var modalBox = document.getElementById("finnToernBox");
+  var modalHeader = document.getElementById("finnToernHeader");
+  var modalImg = document.getElementById("finnToernImg");
+  modal.style.display = "block";
+  modalBox.style.display = "block";
+  modalHeader.style.display = "block";
+  modalImg.style.display = "block";
+}
