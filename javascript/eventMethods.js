844c1fcd92f892db3a67d390c157fb7043b47a36
Oisov <Oistein.Sovik.92@gmail.com>
2017-11-07 00:50:38 +0100

    Renamed javascript files for events, and seperated out all methods

--- javascript/eventMethods.js
+++ javascript/eventMethods.js
@@ -214,34 +214,3 @@ function createEventList(id, eventList) {
 
     }
 }
-
-
-
-fetch(eventUrl).then(function(response) {
-    // Convert to JSON
-    return response.json();
-}).then(function(j) {
-    // Yay, `j` is a JavaScript object
-
-    var events = parseJSON2list(j.events.data);
-
-    /* console.log(events["0"].start);*/
-    /* console.log(events["0"].start.setDate(events["0"].start.getDate()-8));*/
-    /* console.log(events["0"].start.setHours(events["0"].start.getHours()-15));*/
-    /* console.log(events["0"].end.setDate(events["0"].end.getDate()-8));*/
-    /* console.log(events["0"].end.setHours(events["0"].end.getHours()-15));*/
-
-    var futureEvents = getFutureEvents(events);
-
-    var nowEvent = getNowEvent(events);
-    var pastEvents = getPastEvents(events);
-    var nowAndFutureEvents = nowEvent.push.apply(nowEvent, futureEvents);
-
-    createEventList("pastEventsList", pastEvents);
-    createEventList("futureEventsList", futureEvents);
-
-    /* futureEvents = [];*/
-    /* nextEvent = [];*/
-    /* nowEvent = [];*/
-
-});
