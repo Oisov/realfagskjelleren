5a782fb90f9f684b8efeb1b38c7b3c55e57de9e4
Knutner <knut.nerga@gmail.com>
2017-11-13 18:46:49 +0100

    La til modularitet på footer

---
+++ javascript/footer.js
@@ -0,0 +1,36 @@
+var div = document.createElement("div");
+div.id = "images";
+
+var a = makeLink("https://www.facebook.com/Realfagskjelleren");
+var img = makeImage("./img/facebook_new.png", "Facebook", "footerlogo");
+a.appendChild(img);
+div.appendChild(a);
+
+a = makeLink("https://www.instagram.com/explore/tags/rkjeller/");
+a.appendChild(makeImage("./img/temp_instagram.png", "Instagram", "footerlogo"));
+div.appendChild(a);
+
+a = makeLink("mailto:realfagskjellern-styre@list.stud.ntnu.no");
+a.appendChild(makeImage("./img/email.png", "Mail", "footerlogo"));
+div.appendChild(a);
+
+var container = document.getElementById("footer");
+var hr = document.createElement("hr");
+container.appendChild(hr);
+container.appendChild(div);
+
+
+function makeLink(href){
+  var a = document.createElement("a");
+  a.href=href;
+  a.target="_blank";
+  return a;
+}
+
+function makeImage(src, alt, c){
+  var img = document.createElement("img");
+  img.src = src;
+  img.alt = alt;
+  img.classList.add(c);
+  return img;
+}
